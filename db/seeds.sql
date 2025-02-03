ea86f77fc8e60fdf154eaff4df4ef39155c3aa67
Carmen Wheeler <carmenwh33l3r@gmail.com>
2025-02-03 11:03:32 -0600

    Added to seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,3 +9,4 @@ INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
 ('John', 'Doe', 1, NULL),
 ('Jane', 'Smith', 2, 1),
 ('Bill', 'Johnson', 3, 1);
+('Carmen', 'Wheeler', 1, 1);

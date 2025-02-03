e411a59eaa6b39bf53c12dd6cbea74fd734f41b4
Carmen Wheeler <carmenwh33l3r@gmail.com>
2025-02-03 11:38:22 -0600

    updating commits

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,5 +8,6 @@ INSERT INTO role (title, salary, department_id) VALUES
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
 ('John', 'Doe', 1, NULL),
 ('Jane', 'Smith', 2, 1),
-('Bill', 'Johnson', 3, 1);
-('Carmen', 'Wheeler', 1, 1);
+('Bill', 'Johnson', 3, 3);
+('Carmen', 'Wheeler', 1, 2);
+
